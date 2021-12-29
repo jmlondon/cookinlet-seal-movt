2b7fb0badbdbf956906da9e7118626661eac09d3
Josh.London <josh.london@noaa.gov>
2021-12-28 17:25:53 -0800

    add speed filter step to pipeline

---
+++ R/speed_filter_locs.R
@@ -0,0 +1,24 @@
+speed_filter_locs <- function(locs_data) {
+  
+  locs_data <- locs_data %>% 
+    sf::st_drop_geometry() %>% 
+    group_by(deployid) %>%
+    nest() %>%
+    mutate(n_records = map_int(data,nrow)) %>%
+    filter(n_records > 5) 
+  
+  locs_data <- locs_data %>%
+    mutate(filtered = purrr::map(data, ~ argosfilter::sdafilter(
+      lat = .x$latitude,
+      lon = .x$longitude,
+      dtime = .x$locs_dt,
+      lc = as.character(.x$quality),
+      vmax = 7.5 ,ang = -1 
+    ))) %>%
+    unnest(cols = c(data, filtered)) %>%
+    mutate(filtered = ifelse(filtered != "removed","retained",filtered)) %>% 
+    ungroup() %>% select(-quality)
+  
+  locs_data <- st_as_sf(locs_data, coords=c('longitude','latitude'), crs = 4326) %>%
+    st_transform(3338)
+}
