eea598d79a9caa91710bafd8319c3cae5d9c1658
Josh.London <josh.london@noaa.gov>
2021-12-28 17:32:39 -0800

    add speed_filter; delete tidy
    this closed #3 as it's not longer needed

--- _targets.R
+++ _targets.R
@@ -26,10 +26,10 @@ tar_option_set(packages = c(
 # End this file with a list of target objects.
 list(
   tar_target(locs_data, get_ci_data("locs")),
-  tar_target(timeline_data, get_ci_data("timelines"))
-  # tar_target(tidy_data, tidy_ci_data()),
-  # tar_target(fits, fit_crawl()),
-  # tar_target(refits, refit_crawl()),
-  # tar_target(paths, reroute_paths()),
-  # tar_target(output_file,create_output_data(), format="file")
+  tar_target(timeline_data, get_ci_data("timelines")),
+  tar_target(locs_data_filt, speed_filter_locs()),
+  tar_target(fits, fit_crawl()),
+  tar_target(refits, refit_crawl()),
+  tar_target(paths, reroute_paths()),
+  tar_target(output_file,create_output_data(), format="file")
 )
